9bb453f6658535f8a70dba33667dde4a55b7536f
zubair <mzubairnasim37@gmail.com>
2020-09-15 22:21:15 +0500

    add all files

--- backend/key.js
+++
@@ -1,5 +0,0 @@
-
-module.exports={
-    mongourl:process.env.mongourl || "mongodb+srv://zubair:12345@cluster0.vgwvx.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    JWT_SECRET: process.env.JWT_SECRET || "assdsdasa"
-}
